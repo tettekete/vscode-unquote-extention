568de694f02e7cce41f3328b4ed7dff03ce8ce99
tettekete <6535634+tettekete@users.noreply.github.com>
2024-08-31 20:19:19 +0900

    update: implement the unquote function

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,103 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {QuotedStringWithPos} from './util';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const disposable = vscode.commands.registerCommand('vscode-unquote.unquote', () =>
+	{
+		const config = vscode.workspace.getConfiguration('vscode-unquote.unquote');
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {return;}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "unquote" is now active!');
+		const document = editor.document;
+		const selection = editor.selection;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('unquote.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Unquote!');
+		if ( selection.isEmpty )
+		{
+			if( ! config.get<boolean>('unquote_without_selection') )
+			{
+				vscode.window.showInformationMessage( "Config unquote_without_selection is false" );
+				return;
+			}
+			// 選択範囲がないのでカーソル位置を内包するクォートされたテキストを探します
+			// Since there is no selection, it will look for quoted text
+			// that contains the cursor position.
+			const cursorPosition = selection.active;
+			const lineText = document.lineAt(cursorPosition.line).text;
+
+			const r = QuotedStringWithPos( lineText ,cursorPosition.character );
+
+			if( r )
+			{
+				const unquoted = r.quoted_string.substring(1,r.quoted_string.length -1 );
+				const range_begin = new vscode.Position(
+					cursorPosition.line,
+					r.start
+				);
+
+				const range_end = new vscode.Position(
+					cursorPosition.line,
+					r.end
+				);
+
+				editor.edit(editBuilder =>
+				{
+					editBuilder.replace(
+						new vscode.Range(
+							range_begin,
+							range_end
+						)
+						,unquoted
+					);
+				})
+				.then(()=>
+				{
+					const newSelection = new vscode.Selection(
+						range_begin,
+						range_end.translate(0, -2)
+					);
+
+					editor.selection = newSelection;
+				});
+			}
+			else
+			{
+				vscode.window.showInformationMessage( "Could'nt Unquote." );
+			}
+			
+			
+		}
+		else
+		{
+			// There are selected text.
+			const start	= selection.start;
+			const end	= selection.end;
+
+			const startChar = document.getText(new vscode.Range(start.translate(0, -1), start));
+			const endChar = document.getText(new vscode.Range(end, end.translate(0, 1)));
+
+			if( startChar === endChar && /[\'\".`]/.test( startChar) )
+			{
+				const text = document.getText(selection);
+				editor.edit(editBuilder =>
+				{
+					editBuilder.replace(new vscode.Range(start.translate(0, -1), end.translate(0, 1)), text);
+				})
+				.then(()=>
+				{
+					const newSelection = new vscode.Selection(start.translate(0, -1), end.translate(0, -1));
+					editor.selection = newSelection;
+				});
+			}
+			else
+			{
+				vscode.window.showInformationMessage( "Could not Unquote the selection." );
+			}
+		}
+		
 	});
 
 	context.subscriptions.push(disposable);
