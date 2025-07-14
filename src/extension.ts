01145a317172c223e428101e496689c93097de6c
tettekete <6535634+tettekete@users.noreply.github.com>
2025-07-14 13:43:24 +0900

    fix: support multiple cursors in unquoting (fixes #1)
    - Updated unquoting logic to work correctly with multiple selections

--- src/extension.ts
+++ src/extension.ts
@@ -8,102 +8,128 @@ const _message_time_out = 3500;
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('vscode-unquote.unquote', () =>
+	const disposable = vscode.commands.registerCommand('vscode-unquote.unquote', async () =>
 	{
 		const config = vscode.workspace.getConfiguration('unquote');
 		const editor = vscode.window.activeTextEditor;
 		if (!editor) {return;}
 
 		const document = editor.document;
-		const selection = editor.selection;
+		const newSelections: vscode.Selection[] = [];
+		const errorMessages: string[] = [];
 
-		if ( selection.isEmpty )
+		for(let i = 0 ;i<editor.selections.length;i++)
 		{
-			if( ! config.get<boolean>('unquote_without_selection') )
+			const selection = editor.selections[i];
+			const editOptions = 
 			{
-				vscode.window.setStatusBarMessage( "Unquote: Config unquote_without_selection is false.",_message_time_out );
-				return;
+				undoStopBefore: false,
+				undoStopAfter: false
 			}
-			// 選択範囲がないのでカーソル位置を内包するクォートされたテキストを探します
-			// Since there is no selection, it will look for quoted text
-			// that contains the cursor position.
-			const cursorPosition = selection.active;
-			const lineText = document.lineAt(cursorPosition.line).text;
 
-			const r = QuotedStringWithPos( lineText ,cursorPosition.character );
-
-			if( r )
+			if( i === editor.selections.length -1 )
 			{
-				const unquoted = r.quoted_string.substring(1,r.quoted_string.length -1 );
-				const range_begin = new vscode.Position(
-					cursorPosition.line,
-					r.start
-				);
+				editOptions.undoStopAfter = true;
+			}
 
-				const range_end = new vscode.Position(
-					cursorPosition.line,
-					r.end
-				);
-
-				editor.edit(editBuilder =>
+			if( selection.isEmpty )
+			{
+				if( ! config.get<boolean>('unquote_without_selection') )
 				{
-					editBuilder.replace(
-						new vscode.Range(
+					errorMessages.push( "Unquote: Config unquote_without_selection is false." );
+					continue;
+				}
+				// 選択範囲がないのでカーソル位置を内包するクォートされたテキストを探します
+				// Since there is no selection, it will look for quoted text
+				// that contains the cursor position.
+				const cursorPosition = selection.active;
+				const lineText = document.lineAt(cursorPosition.line).text;
+
+				const r = QuotedStringWithPos( lineText ,cursorPosition.character );
+
+				if( r )
+				{
+					const unquoted = r.quoted_string.substring(1,r.quoted_string.length -1 );
+					const range_begin = new vscode.Position(
+						cursorPosition.line,
+						r.start
+					);
+
+					const range_end = new vscode.Position(
+						cursorPosition.line,
+						r.end
+					);
+
+					const replacer = ( editBuilder: vscode.TextEditorEdit ) =>
+					{
+						editBuilder.replace(
+							new vscode.Range(
+								range_begin,
+								range_end
+							)
+							,unquoted
+						);
+					}
+
+					if( await editor.edit( replacer ,editOptions ) )
+					{
+						const newSelection = new vscode.Selection(
 							range_begin,
-							range_end
-						)
-						,unquoted
-					);
-				})
-				.then(()=>
+							range_end.translate(0, -2)
+						);
+
+						newSelections.push( newSelection );
+					};
+				}
+				else
 				{
-					const newSelection = new vscode.Selection(
-						range_begin,
-						range_end.translate(0, -2)
-					);
-
-					editor.selection = newSelection;
-
-					vscode.window.setStatusBarMessage( "Unquoted!" ,_message_time_out);
-				});
+					errorMessages.push( "Unquote: Could'nt Unquote." );
+					continue;
+				}
 			}
 			else
 			{
-				vscode.window.setStatusBarMessage( "Unquote: Could'nt Unquote." ,_message_time_out);
+				// There are selected text.
+				const start	= selection.start;
+				const end	= selection.end;
+
+				const startChar = document.getText(new vscode.Range(start.translate(0, -1), start));
+				const endChar = document.getText(new vscode.Range(end, end.translate(0, 1)));
+
+				if( startChar === endChar && /[\'\".`]/.test( startChar) )
+				{
+					const text = document.getText(selection);
+					const replacer = (editBuilder: vscode.TextEditorEdit ) =>
+					{
+						editBuilder.replace(new vscode.Range(start.translate(0, -1), end.translate(0, 1)), text);
+					}
+
+					if( await editor.edit( replacer ,editOptions ) )
+					{
+						const newSelection = new vscode.Selection(start.translate(0, -1), end.translate(0, -1));
+						newSelections.push( newSelection );
+					};
+				}
+				else
+				{
+					errorMessages.push( "Unquote: Could not Unquote the selection." );
+					continue;
+				}
 			}
-			
-			
 		}
-		else
+
+		if( newSelections.length )
 		{
-			// There are selected text.
-			const start	= selection.start;
-			const end	= selection.end;
-
-			const startChar = document.getText(new vscode.Range(start.translate(0, -1), start));
-			const endChar = document.getText(new vscode.Range(end, end.translate(0, 1)));
-
-			if( startChar === endChar && /[\'\".`]/.test( startChar) )
-			{
-				const text = document.getText(selection);
-				editor.edit(editBuilder =>
-				{
-					editBuilder.replace(new vscode.Range(start.translate(0, -1), end.translate(0, 1)), text);
-				})
-				.then(()=>
-				{
-					const newSelection = new vscode.Selection(start.translate(0, -1), end.translate(0, -1));
-					editor.selection = newSelection;
-
-					vscode.window.setStatusBarMessage( "Unquoted!",_message_time_out );
-				});
-			}
-			else
-			{
-				vscode.window.setStatusBarMessage( "Unquote: Could not Unquote the selection." ,_message_time_out);
-			}
+			editor.selections = newSelections;
+			vscode.window.setStatusBarMessage( "Unquoted!",_message_time_out );
 		}
-		
+		else if( errorMessages.length )
+		{
+			vscode.window.setStatusBarMessage(
+				errorMessages.join("\n")
+				,_message_time_out
+			);
+		}
 	});
 
 	context.subscriptions.push(disposable);
